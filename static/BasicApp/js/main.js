37e7f5c3313961b152f67fdbc581e9e3d814d363
sc495 <sc495@duke.edu>
2018-07-24 15:23:27 -0400

    screen fixed a little bit

--- static/BasicApp/js/main.js
+++ static/BasicApp/js/main.js
@@ -85,6 +85,10 @@ var GameState = {
   //executed after everything is loaded
   create: function() {
 
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
 
     //*Draw Background
     background = this.game.add.sprite(0, 0, 'background'); // two roles here : one is to add sprite, the other one is to assign reference
